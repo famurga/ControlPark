33591a657e7376a6b13a1070f41aec811190e169
famurga <fmurga@unsa.edu.pe>
2021-06-27 15:55:35 -0500

    mapa

--- app/src/main/java/com/example/controlpark/MenuLocalFragment.java
+++ app/src/main/java/com/example/controlpark/MenuLocalFragment.java
@@ -1,16 +1,23 @@
 package com.example.controlpark;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -21,12 +28,18 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
 public class MenuLocalFragment extends Fragment implements OnMapReadyCallback {
 
     GoogleMap mgoogleMap;
     MapView mapView;
     View mview;
-    Button btnPasarDato;
+
+
+    TextView  txtLatitud;
+    TextView txtLongitud;
+    Button gps;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,6 +52,41 @@ public class MenuLocalFragment extends Fragment implements OnMapReadyCallback {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         mview= inflater.inflate(R.layout.fragment_menu_local, container, false);
+
+
+        int permisoUbicacion = ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION);
+        int permisoUbicacion2 = ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        if (permisoUbicacion != PackageManager.PERMISSION_GRANTED || permisoUbicacion2
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+
+
+            } else {
+                ActivityCompat.requestPermissions(getActivity(),new String[]{ Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION},1);
+
+            }
+        }
+
+
+
+        txtLatitud = mview.findViewById(R.id.txtLati);
+        txtLongitud = mview.findViewById(R.id.txtLon);
+        gps = mview.findViewById(R.id.btngps);
+        gps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                miLocalizacion();
+            }
+        });
+
+
+
+
+
 
         return mview;
     }
@@ -79,5 +127,41 @@ public class MenuLocalFragment extends Fragment implements OnMapReadyCallback {
 
 
 
+    }
+
+    public void miLocalizacion(){
+
+        LocationManager locationManager = (LocationManager) getActivity().
+                getSystemService(Context.LOCATION_SERVICE);
+
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                txtLatitud.setText(String.valueOf(location.getLatitude()));
+                txtLongitud.setText(String.valueOf(location.getLongitude()));
+
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        int permision = ContextCompat.checkSelfPermission(getActivity(), ACCESS_FINE_LOCATION);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0,
+                locationListener);
     }
 }
