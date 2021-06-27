4141973274430052f905c118f96a2d39fe856c74
famurga <fmurga@unsa.edu.pe>
2021-06-27 15:44:46 -0500

    tercer Commit

--- app/src/main/java/com/example/controlpark/MainActivity.java
+++ app/src/main/java/com/example/controlpark/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.controlpark;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
 public class MainActivity extends AppCompatActivity {
+    final  private  int REQUEST_CODE_PERMISION=111;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        solicitarpermisos();
+    }
+
+    public void solicitarpermisos(){
+
+        int permisoUbicacion= ActivityCompat.checkSelfPermission(MainActivity.this, ACCESS_FINE_LOCATION);
+        int permisoUbicacion2= ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        if(permisoUbicacion != PackageManager.PERMISSION_GRANTED || permisoUbicacion2 != PackageManager.PERMISSION_GRANTED){
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                requestPermissions(new String[]{ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},REQUEST_CODE_PERMISION);
+
+
+
+            }
+        }
+
+
+
     }
 }
--- app/src/main/java/com/example/controlpark/MenuLocalFragment.java
+++ app/src/main/java/com/example/controlpark/MenuLocalFragment.java
@@ -1,15 +1,32 @@
 package com.example.controlpark;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class MenuLocalFragment extends Fragment {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class MenuLocalFragment extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap mgoogleMap;
+    MapView mapView;
+    View mview;
+    Button btnPasarDato;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,6 +38,46 @@ public class MenuLocalFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu_local, container, false);
+        mview= inflater.inflate(R.layout.fragment_menu_local, container, false);
+
+        return mview;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mapView = (MapView) mview.findViewById(R.id.map);
+        if (mapView != null) {
+            mapView.onCreate(null);
+            mapView.onResume();
+            mapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+
+
+        MapsInitializer.initialize(getContext());
+
+        mgoogleMap = googleMap;
+        mgoogleMap.setMyLocationEnabled(true);
+
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(-16.3846,-71.5243695)).title("MI POSICION ALEATORIA").snippet("ewrwerwerw"));
+        CameraPosition Liberty = CameraPosition.builder().target(new LatLng(-16.3846,-71.5243695)).zoom(18).bearing(0).tilt(45).build();
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(Liberty));
+
+
+
+
     }
 }
