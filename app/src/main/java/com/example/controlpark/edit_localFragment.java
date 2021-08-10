a25305a189e23ef2a090bc5d88f36e4425610963
famurga <fmurga@unsa.edu.pe>
2021-08-10 10:28:24 -0500

    Ultimo commit  h

--- app/src/main/java/com/example/controlpark/edit_localFragment.java
+++ app/src/main/java/com/example/controlpark/edit_localFragment.java
@@ -48,6 +48,7 @@ public class edit_localFragment extends Fragment implements OnMapReadyCallback {
     Button gps;
     DatabaseReference mDataBase;
     double dlati,dlong,dprecio;
+
     int iespacios;
     GoogleMap mgoogleMap;
 
